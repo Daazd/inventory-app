3d36fd1be16cea2dc752616e6b25d0709a6364af
Daazd <zoomie067@yahoo.com>
2024-04-08 11:51:13 -0700

    Deleting process

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
 import { AddItem } from './AddItem';
-
+import { Link, Router, Routes } from 'react-router-dom';
+import 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
+	const [item, setItem] = useState({
+		name: '',
+		description: '',
+		price: '',
+		category: '',
+		image: ''
+	});
+
+	async function fetchItems(){
+		try {
+			const response = await fetch(`${apiURL}/items`);
+			const itemsData = await response.json();
+			
+			setItems(itemsData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
 
 	async function fetchSauces(){
 		try {
@@ -20,6 +40,24 @@ export const App = () => {
 		}
 	}
 
+
+	const handleDelete = async () => {
+		try {
+			const response = await fetch(`${apiURL}/items/${item.id}`, {
+				method: 'DELETE'
+			});
+			if (response.ok) {
+				alert('Item deleted');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchItems();
+	}, []);
+
 	useEffect(() => {
 		fetchSauces();
 	}, []);
@@ -30,6 +68,7 @@ export const App = () => {
 			<h2>All things 🔥</h2>
 			<AddItem />
 			<SaucesList sauces={sauces} />
+			<button onClick={handleDelete}>Delete</button>
 		</main>
 	)
 }
