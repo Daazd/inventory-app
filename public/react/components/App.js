e611b7ed61e0b5daec9f4c965ce897e82e990c46
Taj Smith <TajSmith@Deloitte.com>
2024-04-08 14:48:06 -0400

    Created front end  list view page, model for item,and  express route for getting  all items

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
+	const [currentItem, setCurrentItem] = useState({})
 
-	async function fetchSauces(){
+	async function fetchItems(){
 		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
+			const response = await fetch(`${apiURL}/items`);
+			const itemsData = await response.json();
+			setItems(itemsData);
+			setCurrentItem({});
 		} catch (err) {
-			console.log("Oh no an error! ", err)
+			console.log("Could not find items list ", err)
 		}
 	}
 
+	async function handleItemClick(id){
+		//make it work
+		try {
+		const response = await fetch(`${apiURL}/items/${id}`)
+		const data = await response.json();
+		setCurrentItem(data)
+		setItems([])
+		}
+		catch(err) {
+		  console.log('Error getting the item', err)
+		}
+	  }
+
+	async function handleBackClick() {
+		try {
+			const response = await fetch(`${apiURL}/items`);
+			const itemsData = await response.json();
+			setItems(itemsData);
+			setCurrentItem({});
+		} catch (err) {
+			console.log("Could not find items ", err)
+		}
+	  }
+	
+	  async function handleAddItemClick() {
+		setItems([]);
+		setCurrentItem({});
+		//create form to add Item.
+	  }
+
 	useEffect(() => {
-		fetchSauces();
+		fetchItems();
 	}, []);
 
 	return (
 		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+      <h1>Inventory</h1>
+			<h2>Items:</h2>
+			<ItemsList items={items} handleItemClick={handleItemClick} />
+			{currentItem.name && (
+        <div>
+          <h2>{currentItem.name}</h2>
+		  {currentItem.image && <p>Image: {currentItem.image}</p>}
+          {currentItem.price && <p>Price: {currentItem.price}</p>}
+          {currentItem.description && <p>Description: {currentItem.description}</p>}
+		  {currentItem.category && <p>Category: {currentItem.description}</p>}
+        </div>
+      )}
+       {!currentItem.name && (<button onClick={()=> handleAddItemClick() }>Add Item</button>)}
+      {items.length === 0 && (<button onClick={() => handleBackClick()}> Back to Inventory</button>)}
 		</main>
 	)
 }
--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize')
 const {sequelize} = require('../db')
 
-const Sauce = sequelize.define("sauces", {
+const Item = sequelize.define("items", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
-  Sauce,
+  Item,
 };
