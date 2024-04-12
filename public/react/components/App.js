430f3b5e449d71056860cc7bf3c41e7305a95aa2
rickcedwhat <46553325+rickcedwhat@users.noreply.github.com>
2024-04-12 12:13:22 -0400

    Remove unused code and unnecessary useState from App.js (#44)
    * got rid of unused code in App.js
    
    * removed unnecessary useState from App.js

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -15,54 +15,6 @@ import { AppProvider } from "../contexts/AppContext";
 import apiURL from "../api";
 
 export const App = () => {
-  const [items, setItems] = useState([]);
-  // const [user, setUser] = useState(null);
-  // const [cart, setCart] = useState([]);
-
-  // const getCart = async () => {
-  //   // find cart by user id
-  //   // if it doesnt exist, create one
-  //   // const response = await fetch()...
-  //   // const data = response.json();
-  //   // setCart(data)
-  //   setCart([]);
-  // };
-
-  // const addItemToCart = async ({ item }) => {
-  //   // TODO
-  //   setCart([...cart, item]);
-  // };
-
-  // const removeItemFromCart = async ({ item, cart }) => {
-  //   // TODO
-  // };
-
-  // const emptyCart = async ({ cart }) => {
-  //   // TODO
-  //   setCart([]);
-  // };
-
-  // const incrementItem = async ({ id }) => {
-  //   // TODO
-  //   // const item = cart.find((cartItem) => cartItem.id === id);
-  //   // item.quantity += 1;
-  //   // setCart([...cart]);
-  //   setCart((prev) => {
-  //     const newCart = [...prev];
-  //     const item = newCart.find((cartItem) => cartItem.id === id);
-  //     item.quantity += 1;
-  //     return newCart;
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   if (user) {
-  //     getCart();
-  //   }
-  // }, [user]);
-
-  // const cartMethods = { addItemToCart, removeItemFromCart, emptyCart };
-
   // create router and routes: /, /items, /items/:id, /cart
   const routes = (
     <Routes>
